4e4792fe51d7ab9341b16f3c29c8c3b2df85d4ea
The Degenerate Lord Of Chaos & BBWs <josh14kyle2010@gmail.com>
2025-11-09 17:15:07 +0000

    -----BEGIN PGP PUBLIC KEY BLOCK----- mDMEaRDKxxYJKwYBBAHaRw8BAQdA0STJpnJ

--- src/components/calculator-page.tsx
+++ src/components/calculator-page.tsx
@@ -545,9 +545,18 @@ export function CalculatorPage() {
                                 <pre className="text-xs font-code whitespace-pre-wrap bg-secondary/30 p-4 rounded-lg overflow-x-auto">
                                     <code>
 -----BEGIN PGP PUBLIC KEY BLOCK-----
-
-[PASTE YOUR PGP PUBLIC KEY BLOCK HERE]
-
+mDMEaRDKxxYJKwYBBAHaRw8BAQdA0STJpnJMGAoX+qXAjcsK0NOEFd7O5SqnsAqh
+OO8RGv60WkZpbG0gRGVnZW5lcmF0ZXMgTExDICYgV2FyU2V4RHJ1Z3NEb3RDb20g
+TExDIDxUaGVEZWdlbmVyYXRlTG9yZG9mQ2hhb3NARG9wZURvb2hpY2tleXMuY29t
+PoiZBBMWCgBBFiEEyeW7pvMwLNUGtLI/yRO7jX5CNUwFAmkQyscCGwMFCQWk6ckF
+CwkIBwICIgIGFQoJCAsCBBYCAwECHgcCF4AACgkQyRO7jX5CNUyr1gEAuChJ/OvG
+l+Htre1yP1sz6Or8AXPRW0h9hdpdGKpUy3gBAL4qK3Tb4xOUIgYk+l23SK0i6NtZ
+ykBKggp4pjlDK+QAuDgEaRDKxxIKKwYBBAGXVQEFAQEHQIH1SqAgnfi55MIl2lcK
+fZdhGA0NNO+xKmWDbYQP2wZrAwEIB4h+BBgWCgAmFiEEyeW7pvMwLNUGtLI/yRO7
+jX5CNUwFAmkQyscCGwwFCQWk6ckACgkQyRO7jX5CNUw83gD/cnE3YELrmsX6sK98
+h/2iJ8LzRZqW+vxP/XGPa2aAFwUBAPkPo8fNVsGmrMmDwZmjoBipE5UJs3YXa17/
+UgsxjwcC
+=UYLD
 -----END PGP PUBLIC KEY BLOCK-----
                                     </code>
                                 </pre>
