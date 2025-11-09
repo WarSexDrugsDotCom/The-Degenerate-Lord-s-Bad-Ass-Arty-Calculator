214a24ea7fa7256c8be93edb1b443f4ba04a062a
The Degenerate Lord Of Chaos & BBWs <josh14kyle2010@gmail.com>
2025-11-09 13:02:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8120607224-6ebcd",
+  "appId": "1:233760160747:web:4038e2aeb69ba2ed9837ae",
+  "apiKey": "AIzaSyD5amA_JEXKWKpybHmW8nZ_Brw0yeuseL4",
+  "authDomain": "studio-8120607224-6ebcd.firebaseapp.com",
+  "measurementId": "G-Q7714QF2GZ",
+  "messagingSenderId": "233760160747"
+};
